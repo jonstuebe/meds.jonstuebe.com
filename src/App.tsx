7a002143458feb44a653856d0a513db0cf95679c
Jon Stuebe <jstuebe@gmail.com>
2021-05-07 13:51:34 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { Flex, Heading, Button } from "@chakra-ui/react";
+import { useCallback, useMemo, useState } from "react";
+import { parseISO, isBefore, startOfDay, isToday } from "date-fns";
+
+const useMeds = () => {
+  const [lastTaken, set] = useState(() =>
+    localStorage.getItem("lastMeds")
+      ? parseISO(localStorage.getItem("lastMeds") as string)
+      : undefined
+  );
+
+  const setTaken = useCallback(() => {
+    const time = new Date();
+    localStorage.setItem("lastMeds", time.toISOString());
+    set(time);
+  }, []);
+
+  const taken = useMemo(() => {
+    if (lastTaken && isToday(lastTaken)) {
+      return true;
+    }
+
+    return false;
+  }, [lastTaken]);
+
+  return {
+    taken,
+    setTaken
+  };
+};
+
+export default function App() {
+  const { taken, setTaken } = useMeds();
+
+  const onClick = useCallback(() => {
+    // console.log("taken =>", taken);
+    setTaken();
+  }, [setTaken]);
+
+  return (
+    <>
+      <Flex justify="center" align="center" h="100vh">
+        {taken ? (
+          <Heading>Meds Taken</Heading>
+        ) : (
+          <Button colorScheme="blue" onClick={onClick}>
+            Took my meds
+          </Button>
+        )}
+      </Flex>
+    </>
+  );
+}
